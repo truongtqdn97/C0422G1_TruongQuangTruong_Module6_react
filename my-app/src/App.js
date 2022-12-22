2f15b7139bf3e4adaa673b8a026c1629f952091d
truongtqdn97 <truong.dn97@gmail.com>
2022-12-22 17:03:52 +0700

    crud

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
             index: 0,
             name: "",
             price: 0,
-            action: "ADD",
+            action: "THÊM MỚI",
             items: [
                 {
                     name: "Phao câu gà",
@@ -50,46 +50,128 @@ class App extends React.Component {
         })
     }
 
+    addItem = () => {
+        if (!this.state.items.find(item => item.name === this.state.name)) {
+            this.setState({
+                items: [
+                    ...this.state.items,
+                    {
+                        name: this.state.name,
+                        price: this.state.price
+                    }
+                ],
+                name: "",
+                price: ""
+            })
+        }
+    }
+
+    edit = (item, index) => {
+        this.setState({
+            action: 'CHỈNH SỬA',
+            name: item.name,
+            price: item.price,
+            index: index
+        });
+    }
+
+    updateItem = () => {
+        let data = this.state.items;
+        data.map((item, index) => {
+            if (this.state.index === index) {
+                item.name = this.state.name;
+                item.price = parseInt(this.state.price);
+            }
+        })
+        this.setState({
+            items: data,
+            name: "",
+            price: "",
+            action: 'THÊM MỚI'
+        })
+    }
+
+    deleteItem = (name) => this.setState({
+        items: this.state.items.filter(item => item.name != name)
+    })
+
     render() {
         return (
-            <div className="container">
-                <Table className="mt-3"
-                    hover
-                    responsive
-                    striped
-                >
-                    <thead>
-                    <tr className="table-primary">
-                        <th>
-                            #
-                        </th>
-                        <th>
-                            Tên sản phẩm
-                        </th>
-                        <th>
-                            Giá
-                        </th>
-                        <th colSpan="2" className="align-content-center">
-                            Hành động
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        this.state.items.map((item, index)=>(
-                            <tr key={index}>
-                                <th scope="row">
-                                    {index+1}
+            <div className="container mt-3">
+                <div className="row">
+                    <div className="col-8">
+                        <h2 className="d-flex justify-content-center">NỘI TẠNG QUÁN</h2>
+                        <Table className="mt-3"
+                               hover
+                               responsive
+                               striped
+                        >
+                            <thead>
+                            <tr className="table-primary">
+                                <th>
+                                    #
                                 </th>
-                                <td>{item.name}</td>
-                                <td>{item.price}</td>
-                                <td><Button color="success" outline>Update</Button></td>
-                                <td><Button color="danger" outline>Remove</Button></td>
+                                <th>
+                                    Tên sản phẩm
+                                </th>
+                                <th>
+                                    Giá
+                                </th>
+                                <th>
+                                    Sửa
+                                </th>
+                                <th>
+                                    Xoá
+                                </th>
                             </tr>
-                        ))
-                    }
-                    </tbody>
-                </Table>
+                            </thead>
+                            <tbody>
+                            {
+                                this.state.items.map((item, index) => (
+                                    <tr key={index}>
+                                        <th scope="row">
+                                            {index + 1}
+                                        </th>
+                                        <td>{item.name}</td>
+                                        <td>{item.price}</td>
+                                        <td>
+                                            <Button color="success" outline
+                                            onClick={()=>this.edit(item,index)}>
+                                                Sửa
+                                            </Button>
+                                        </td>
+                                        <td>
+                                            <Button color="danger" outline
+                                            onClick={()=>this.deleteItem(item.name)}>
+                                                Xoá
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                            </tbody>
+                        </Table>
+                    </div>
+                    <div className="col-4">
+                        <h1>{this.state.action}</h1>
+                        <div className="form-group">
+                            <label>Tên nội tạng</label>
+                            <input type="text" name="" className="form-control" onChange={this.changeName}
+                                   value={this.state.name}/>
+                        </div>
+                        <div className="form-group">
+                            <label>Giá</label>
+                            <input type="text" name="" className="form-control" onChange={this.changePrice}
+                                   value={this.state.price}/>
+                        </div>
+                        <div className="form-group">
+                            <button type="button" className="btn btn-primary"
+                                    onClick={this.state.action == 'THÊM MỚI' ? this.addItem : this.updateItem}>OK
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         );
     }
